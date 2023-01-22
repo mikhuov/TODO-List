f3d4b680afd81e36c9358b266e27028f8e3460a7
mikhuov <mikhuovinen@gmail.com>
2023-01-22 18:11:32 +0200

    Added: Beginning Framework For the Project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TODO-List
 TO00BL10-3018
+
+TODO-List Project for Laurea Universy of Applied Sciences Course TO00BL10-3018
---
+++ index.js
@@ -0,0 +1,7 @@
+function add() {
+    //Adds items to toDoList
+}
+
+function remove() {
+    //removes items from the toDoList
+}
