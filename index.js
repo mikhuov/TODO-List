af643e54f99832e52cfc7c1765f4940f24e99b0e
mikhuov <mikhuovinen@gmail.com>
2023-01-23 11:15:04 +0200

    Added: Progress

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 function add() {
     //Adds items to toDoList
+    createLiElement();
+}
+
+function createButtons(li) {
+    var span = document.createElement("SPAN");
+    var button = document.createElement("INPUT");
+    button.setAttribute("type", "radio");
+    span.className = "close";
+    span.appendChild(button);
+    li.appendChild(span);
+}
+
+function createLiElement(){
+    let li = document.createElement("li");
+    let value = document.getElementById("listInput").value;
+    console.log('value: ', value);
+
+    if(value.length < 5) {
+        alert("Item cannot have less than 5 characters")
+    } else {
+        let text = document.createTextNode(value);
+        li.appendChild(text);
+        document.getElementById("toDoList").appendChild(li);
+        createButtons(li);
+    }
 }
 
 function remove() {
