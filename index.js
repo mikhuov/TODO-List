9b0f7e0b3299d8a35af2aad9803963ec2f736c09
mikhuov <mikhuovinen@gmail.com>
2023-01-24 21:27:15 +0200

    Added: Progress

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
+const data = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+const list = document.getElementById("toDoList");
+
+localStorage.setItem('items', JSON.stringify(data));
+
+data.forEach(createLiElement);
+
 function add() {
     //Adds items to toDoList
-    createLiElement();
-}
+    //Call createLiElement
 
-function createButtons(li) {
-    var span = document.createElement("SPAN");
-    var button = document.createElement("INPUT");
-    button.setAttribute("type", "radio");
-    span.className = "close";
-    span.appendChild(button);
-    li.appendChild(span);
-}
-
-function createLiElement(){
-    let li = document.createElement("li");
-    let value = document.getElementById("listInput").value;
-    console.log('value: ', value);
+    const li = document.createElement("li");
+    const value = document.getElementById("listInput").value;
 
     if(value.length < 5) {
         alert("Item cannot have less than 5 characters")
@@ -23,10 +18,56 @@ function createLiElement(){
         let text = document.createTextNode(value);
         li.appendChild(text);
         document.getElementById("toDoList").appendChild(li);
+        //Call create Buttons
         createButtons(li);
+        data.push([value]);
+        
+        localStorage.setItem('items', JSON.stringify(data));
     }
+}
+
+function checkOff() {
+    const ul = document.getElementById("toDoList");
+    ul.addEventListener("click", event => {
+        if (event.target.tagName === "LI" ) {        
+            event.target.classList.toggle("checked");
+        }
+    });
+}
+
+function createButtons(li) {
+    //Creates Buttons for Li-elements for additional funcionalities
+    const span = document.createElement("SPAN");
+    const button = document.createElement("BUTTON");
+
+    span.className ="buttonSpan";
+    button.className ="selectButton";
+    button.onclick = remove;
+
+    span.appendChild(button);
+    li.appendChild(span);
+}
+
+function createLiElement(textContent){
+    //Creates Li-element with the necessary values
+    const li = document.createElement("LI");
+    li.textContent = textContent;
+    createButtons(li);
+    list.appendChild(li);
 }
 
 function remove() {
     //removes items from the toDoList
+    const buttonSpan = document.getElementsByClassName("buttonSpan");
+    var i;
+    for (i = 0; i < buttonSpan.length; i++) {
+        buttonSpan[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+
+        let index = buttonSpan[i].indexOf()
+    
+        data.splice(index, 1);
+        }
+    }
 }
